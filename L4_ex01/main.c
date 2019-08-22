5337a9e7db25ee00f7e539a77934d78fb45642ea
jeancabraldev <54410614+jeancabraldev@users.noreply.github.com>
2019-08-22 14:27:04 -0300

    Add files via upload

---
+++ L4_ex01/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   //1.	Faça um programa que exibe a tabuada do dois.
+
+   int x;
+
+   for(x = 0; x <= 10; x++){
+        printf("\n2  X %2d = %2d", x, x * 2);
+   }
+    return 0;
+}
